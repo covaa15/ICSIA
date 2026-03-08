7f1c7849721baca63e54f648a4555d533789b064
covaa15 <covadongabustantigo@gmail.com>
2026-03-08 13:17:22 +0100

    Completado

--- Ejer_10_2/Ejercicio1/src/main.jsx
+++ Ejer_10_2/Ejercicio1/src/main.jsx
@@ -5,7 +5,7 @@ import { RouterProvider } from 'react-router-dom'
 import router from './app.jsx'
 import './css/style.css'
 
-// Inicio la aplicación cargando el router
+// Inicio la aplicacion cargando el router
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
